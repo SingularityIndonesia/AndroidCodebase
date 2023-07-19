7f26b8193c26031f1e740b8be5790c49f0eff713
stefanus <stefanus.ayudha@gmail.com>
2023-07-19 14:10:47 +0700

    add regex

--- codebase/src/main/java/com/singularity_code/codebase/util/Regex.kt
+++ codebase/src/main/java/com/singularity_code/codebase/util/Regex.kt
@@ -6,4 +6,7 @@
 
 package com.singularity_code.codebase.util
 
+@Deprecated("use NOT_EMPTY_PATTERN_INSTEAD")
 val NOT_EMPTY get() = Regex("^(?!\\s*\$).+")
+val NOT_EMPTY_PATTERN get() = Regex("^(?!\\s*\$).+")
+val EMAIL_PATTERN get() = Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}\$")
