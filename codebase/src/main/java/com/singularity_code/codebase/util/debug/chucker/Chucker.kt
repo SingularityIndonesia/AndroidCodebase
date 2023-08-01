70366a82442ccb5970d8015dcc49e4bc00179e30
stefanus <stefanus.ayudha@gmail.com>
2023-08-01 15:51:31 +0700

    fix type unresolved

--- codebase/src/main/java/com/singularity_code/codebase/util/debug/chucker/Chucker.kt
+++ codebase/src/main/java/com/singularity_code/codebase/util/debug/chucker/Chucker.kt
@@ -3,6 +3,7 @@ package com.singularity_code.codebase.util.debug.chucker
 import com.chuckerteam.chucker.api.ChuckerCollector
 import com.chuckerteam.chucker.api.RetentionManager
 import com.singularity_code.codebase.util.Singularity
+import okhttp3.Interceptor
 import com.chuckerteam.chucker.api.ChuckerInterceptor as Chucker
 
 // Create the Collector
@@ -15,7 +16,7 @@ val chuckerCollector = ChuckerCollector(
 )
 
 // Create the Interceptor
-val ChuckerInterceptor = Chucker.Builder(Singularity.application)
+val ChuckerInterceptor: Interceptor = Chucker.Builder(Singularity.application)
     // The previously created Collector
     .collector(chuckerCollector)
     // The max body content length in bytes, after this responses will be truncated.
