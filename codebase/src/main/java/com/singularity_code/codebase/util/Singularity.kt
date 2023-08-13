56d8711fe596bf236a398835c1cc6e56f43da43c
stefanus <stefanus.ayudha@gmail.com>
2023-08-13 10:46:24 +0700

    enable pluto add pluto handler support

--- codebase/src/main/java/com/singularity_code/codebase/util/Singularity.kt
+++ codebase/src/main/java/com/singularity_code/codebase/util/Singularity.kt
@@ -2,19 +2,24 @@ package com.singularity_code.codebase.util
 
 import android.app.Application
 import androidx.multidex.MultiDex
+import com.pluto.plugins.exceptions.UncaughtANRHandler
 import com.singularity_code.codebase.util.debug.pluto.preparePluto
+import java.lang.Thread.UncaughtExceptionHandler
 
 
 object Singularity {
-    enum class Feature {
-        PLUTO_DEBUGGER,
+    sealed class Feature {
+        class PLUTO_DEBUGGER(
+            val anrHandler: UncaughtANRHandler? = null,
+            val exceptionHandler: UncaughtExceptionHandler? = null
+        ) : Feature()
 
         @Deprecated(
             "Don't worry, just want to make sure that you set `android.defaultConfig.multiDexEnabled = true` "
                     + "in your app build.gradle. "
                     + "If you did, you can ignore this warning."
         )
-        MULTI_DEX
+        class MULTI_DEX : Feature()
     }
 
     lateinit var application: Application
@@ -32,11 +37,21 @@ object Singularity {
         vararg features: Feature
     ): Singularity {
 
-        if (features.contains(Feature.MULTI_DEX))
-            MultiDex.install(application.applicationContext)
+        features.forEach {
+            when (it) {
+                is Feature.MULTI_DEX -> {
+                    MultiDex.install(application.applicationContext)
+                }
 
-        if (features.contains(Feature.PLUTO_DEBUGGER))
-            preparePluto(application)
+                is Feature.PLUTO_DEBUGGER -> {
+                    preparePluto(
+                        application,
+                        it.anrHandler,
+                        it.exceptionHandler
+                    )
+                }
+            }
+        }
 
         _enabledFeatures.addAll(features)
 
