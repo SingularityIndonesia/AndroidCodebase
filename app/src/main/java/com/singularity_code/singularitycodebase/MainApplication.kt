53777e125116059e20406527162cfa3dd8d182be
stefanus <stefanus.ayudha@gmail.com>
2023-09-06 10:35:00 +0700

    add comment

--- app/src/main/java/com/singularity_code/singularitycodebase/MainApplication.kt
+++ app/src/main/java/com/singularity_code/singularitycodebase/MainApplication.kt
@@ -10,6 +10,9 @@ class MainApplication : Application() {
         Singularity
             .Init(this)
             .EnableFeature(
+                // note, pluto debugger not available yet in demo,
+                // but in your code, you can use it by adding pluto module in your project and uncomment this line.
+                // also make sure to implement codebase debug flavour in your project.
                 /*Singularity.Feature.PLUTO_DEBUGGER(),*/
                 Singularity.Feature.MULTI_DEX()
             )
