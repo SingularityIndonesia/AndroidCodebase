83cb260e06422ef6b605b9a338b08aa4b63ca8e1
stefanus <stefanus.ayudha@gmail.com>
2023-09-06 10:40:41 +0700

    provide new semantic: automate

--- codebase/src/main/java/com/singularity_code/codebase/util/flow/Auto.kt
+++ codebase/src/main/java/com/singularity_code/codebase/util/flow/Auto.kt
@@ -3,3 +3,7 @@ package com.singularity_code.codebase.util.flow
 import androidx.lifecycle.ViewModel
 
 fun <R> ViewModel.auto(block: () -> R): Lazy<R> = lazy { block.invoke() }
+
+fun <T> T.automate(block: (T) -> T): T = this.also {
+    block.invoke(it)
+}
