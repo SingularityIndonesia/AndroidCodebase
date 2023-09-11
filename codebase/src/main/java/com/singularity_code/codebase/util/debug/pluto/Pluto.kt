a20f9775edad3d077eef50b81b7b83410c374144
stefanus <stefanus.ayudha@gmail.com>
2023-09-11 13:18:43 +0700

    add more function

--- codebase/src/main/java/com/singularity_code/codebase/util/debug/pluto/Pluto.kt
+++ codebase/src/main/java/com/singularity_code/codebase/util/debug/pluto/Pluto.kt
@@ -1,6 +1,11 @@
 package com.singularity_code.codebase.util.debug.pluto
 
+import android.app.Activity
 import android.app.Application
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import android.widget.Toast
 import com.pluto.Pluto
 import com.pluto.plugins.datastore.pref.PlutoDatastorePreferencesPlugin
 import com.pluto.plugins.exceptions.PlutoExceptions
@@ -12,6 +17,24 @@ import com.pluto.plugins.network.PlutoNetworkPlugin
 import com.pluto.plugins.preferences.PlutoSharePreferencesPlugin
 import timber.log.Timber
 import java.lang.Thread.UncaughtExceptionHandler
+
+context(Activity)
+fun preparePluto() {
+    if (!Settings.canDrawOverlays(this@Activity)) {
+        val intent = Intent(
+            Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+            Uri.parse("package:$packageName")
+        )
+
+        startActivityForResult(intent, 0)
+
+        Toast.makeText(
+            this@Activity,
+            "Allow Pluto to display over window.",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+}
 
 fun preparePluto(
     application: Application,
--- codebase/src/main/java/com/singularity_code/codebase/util/flow/Auto.kt
+++ codebase/src/main/java/com/singularity_code/codebase/util/flow/Auto.kt
@@ -2,6 +2,7 @@ package com.singularity_code.codebase.util.flow
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 fun <R> ViewModel.auto(block: () -> R): Lazy<R> = lazy { block.invoke() }
@@ -9,6 +10,13 @@ fun <R> ViewModel.auto(block: () -> R): Lazy<R> = lazy { block.invoke() }
 context (ViewModel)
 fun <T> T.automate(block: suspend T.() -> Unit): T = this.also {
     viewModelScope.launch {
+        block.invoke(it)
+    }
+}
+
+context (CoroutineScope)
+fun <T> T.automate(block: suspend T.() -> Unit): T = this.also {
+    launch {
         block.invoke(it)
     }
 }
